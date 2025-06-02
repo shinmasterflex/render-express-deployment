ff5280d3f947adb6b6b767de04b4685ae57e4302
shinmasterflex <dshin098@gmail.com>
2025-06-02 18:26:43 -0500

    created package.json and server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Backend Server!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
